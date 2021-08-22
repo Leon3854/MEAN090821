43ed5cd04180f757254afe36626b941bf30d0f38
Leon3854 <frad109@mail.ru>
2021-08-22 20:29:01 +0500

    adding all routes and controllers in server page

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,8 @@ const express = require('express');
 const controller = require('../controllers/auth');
 const router = express.Router();
 
-router.get('/login', controller.login);
+router.post('/login', controller.login);
 
-router.get('/register', controller.register);
+router.post('/register', controller.register);
 
 module.exports = router;
