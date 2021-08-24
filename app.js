086845510ce9dbce02e436d6ad3afa95f5bda004
Leon3854 <frad109@mail.ru>
2021-08-24 21:25:39 +0500

    finish working for the include mongoDb

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
+const keys = require('./config/keys');
 
 const app = express();
+
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+.then(() => console.log('MongoDb connected.'))
+.catch(error => console.log(error));
 
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://Leon3854:175922@cluster0.pjfjd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
