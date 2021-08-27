16caa5625fe03eb4f354e8ec6fab36bb293a7233
Leon3854 <frad109@mail.ru>
2021-08-27 20:07:02 +0500

    Finish working with getByCategoryId in position folder

--- controllers/position.js
+++ controllers/position.js
@@ -1,7 +1,20 @@
 const Position = require('../models/Position');
-const errorHandler = require('../');
+const errorHandler = require('../utils/errorHandler');
 
-module.exports.getByCategoryId = function(req, res) {}
+module.exports.getByCategoryId = async function(req, res) {
+  try {
+    const positions = await Position.find({
+      category: req.params.categoryId,
+      user: req.user.id
+    });
+    res.status(200).json(positions);
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
+
 module.exports.create = function(req, res) {}
+
 module.exports.remove = function(req, res) {}
+
 module.exports.update = function(req, res) {}
